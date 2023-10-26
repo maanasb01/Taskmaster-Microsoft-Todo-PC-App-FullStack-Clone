bd1df65438c12e910e0b4b4acfa7499d5c900cd8
maanasb01 <maanasb01@gmail.com>
2023-10-26 23:42:02 +0530

    minor structural changes in server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const connectToMongo = require("./db");
 
 connectToMongo();
 const app = express();
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 3000;
 
 app.use(
   cors({
